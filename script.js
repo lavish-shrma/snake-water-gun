89db1872082f3085863a1fee243136491c38b2b0
lavish-shrma <lavishkumar3435@gmail.com>
2024-10-19 22:02:22 +0530

    updating the code

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 let playerPick = prompt("Enter your move");
 let cpuI = Math.floor(Math.random() * 3);
-let cpu = ["s","w","g"][cpuI]
+let cpu = ["S","W","G"][cpuI]
+
+const match = (cpu, playerPick) => {
+    if(cpu === playerPick){
+        return 0;
+    } else if(cpu === "S" && playerPick === "W"){
+        return "cpu"
+    } else if(cpu === "S" && playerPick === "G"){
+        return "user"
+    } else if(cpu === "G" && playerPick === "W"){
+        return "user"
+    } else if(cpu === "G" && playerPick === "S"){
+        return "cpu"
+    } else if(cpu === "W" && playerPick === "S"){
+        return "user"
+    } else if(cpu === "W" && playerPick === "G"){
+        return "cpu"
+    }
+}
+
+match(cpu, playerPick)
